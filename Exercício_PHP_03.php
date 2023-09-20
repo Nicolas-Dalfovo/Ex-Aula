fd15d395fe3c22ef3b51154daa6a9c96cfd68eb0
Nicolas-Dalfovo <140664489+Nicolas-Dalfovo@users.noreply.github.com>
2023-09-20 14:31:55 -0300

    Add files via upload

---
+++ Exercício_PHP_03.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $lado = 3;
+
+    $area = pow($lado, 2);
+
+    echo 'A área de um quadrado de lado ', $lado, ' metros é ', $area, ' metros quadrados';   
+
+
+
+    ?>
+</body>
+</html>
