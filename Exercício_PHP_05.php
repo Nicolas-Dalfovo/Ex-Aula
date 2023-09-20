d18fd2add838648074195753cba3e6b118403015
Nicolas-Dalfovo <140664489+Nicolas-Dalfovo@users.noreply.github.com>
2023-09-20 14:53:14 -0300

    Add files via upload

---
+++ Exercício_PHP_05.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <?php
+        
+            $base = 10;
+            $altura = 6;
+
+            $area = ($base * $altura) / 2;
+
+            echo 'a área de um triângulo retângulo com uma base de ', $base, ' metros e uma altura de ', $altura, ' metros é de ', $area, ' metros quadrados';
+        ?>
+</body>
+</html>
