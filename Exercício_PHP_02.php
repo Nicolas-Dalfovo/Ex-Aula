3849dd415eec5c2972a5a26c14aee20cab0648b4
Nicolas-Dalfovo <140664489+Nicolas-Dalfovo@users.noreply.github.com>
2023-09-20 14:16:38 -0300

    Add files via upload

---
+++ Exercício_PHP_02.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $var = 43212;
+
+    if( ($var % 2) == 0){
+
+        echo 'Valor divisível por 2';
+
+    }else{
+
+        echo 'Valor não divisível por 2';
+    }
+
+    
+    
+    
+    ?>
+</body>
+</html>
